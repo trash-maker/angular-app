60ce434a46c119c268bea021ae10a2ace2aeb30a
trash-maker <undesired.mails.here@gmail.com>
2021-01-26 11:06:50 +0100

    fix: scss global theme variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const webpackMerge = require('webpack-merge');
+const webpackMerge = require("webpack-merge");
 
 module.exports = (config) => {
-  const merge = webpackMerge && webpackMerge.merge ? webpackMerge.merge : webpackMerge;
+  const merge =
+    webpackMerge && webpackMerge.merge ? webpackMerge.merge : webpackMerge;
   const isProd = config.mode === "production";
   const tailwindConfig = require("./tailwind.config.js")(isProd);
 
@@ -10,20 +11,20 @@ module.exports = (config) => {
       rules: [
         {
           test: /\.scss$/,
-          loader: 'postcss-loader',
+          loader: "postcss-loader",
           options: {
             postcssOptions: {
-              ident: 'postcss',
-              syntax: 'postcss-scss',
+              ident: "postcss",
+              syntax: "postcss-scss",
               plugins: [
-                require('postcss-import'),
-                require('tailwindcss')(tailwindConfig),
-                require('autoprefixer'),
-              ]
-            }
-          }
-        }
-      ]
-    }
+                require("postcss-import"),
+                require("tailwindcss")(tailwindConfig),
+                require("autoprefixer"),
+              ],
+            },
+          },
+        },
+      ],
+    },
   });
 };
